72e8aa740880029282079fe52a69fae011284600
regbake <regbake@gmail.com>
2017-08-30 18:42:07 -0700

    Reggie Baker

---
+++ js/efficientStore.js
@@ -0,0 +1,16 @@
+var friendz = {
+	Moe: 18,
+	Larry: 19,
+	Curly: 20,
+	Jane: 20,
+	Emma: 21,
+	Elizabeth: 18,
+	Elinor: 23,
+	Mary: 25,
+	Darcy: 24,
+	Grey: 18,
+	Lydia: 24,
+	Harriet: 18,
+}
+
+console.log(friendz.Jane);
---
+++ js/fiveTen.js
@@ -0,0 +1,25 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+var fiveDime = foods.slice(4,11);
+
+console.log(fiveDime);
