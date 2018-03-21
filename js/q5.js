b1d3621ebf5fc44d07bd600447f83d8b7c4e3d6b
Van Ha <hav206@yahoo.com>
2018-03-21 15:54:32 -0700

    not yet finish

---
+++ js/q5.js
@@ -0,0 +1,28 @@
+var myFriends = [
+  'Rickon',
+  'Meera',
+  'Hodor',
+  'Jojen',
+  'Osha',
+  'Rickard',
+  'Maester',
+  'Rodrik',
+  'Jory',
+  'Septa',
+  'Jon'
+];
+
+var yourFriends = [
+  'Bilbo',
+  'Boromir',
+  'Elrond',
+  'Faramir',
+  'Frodo',
+  'Gandalf',
+  'Legolas',
+  'Pippin'
+];
+
+var newArray = myFriends.concat(yourFriends);
+
+console.log(newArray.sort());
---
+++ js/q6.js
@@ -0,0 +1,26 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+]
+
+for(var i = 0; i < foods.length; i++)
+{
+  console.log("rank " + (i + 1) +  " " +foods[i]);
+}
