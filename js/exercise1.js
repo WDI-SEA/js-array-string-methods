a8b37ff3edac7320fb731dcda5d48ae8677fbc0d
bradserlis <bradserlis@yahoo.com>
2018-03-21 15:29:46 -0700

    JS Primitives, before the bonus

---
+++ js/exercise1.js
@@ -0,0 +1,18 @@
+// 1.) Find the last name in the following array:
+
+var friends = [
+  'Moe',
+  'Larry',
+  'Curly',
+  'Jane',
+  'Emma',
+  'Elizabeth',
+  'Elinor',
+  'Mary',
+  'Darcy',
+  'Grey',
+  'Lydia',
+  'Harriet'
+];
+
+friends[friends.length-1];
---
+++ js/exercise8.js
@@ -0,0 +1,29 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+
+// for(var i=4; i<9; i++){
+//   console.log(foods[i]);
+// };
+
+var topFive = foods.slice(4, 9);
+console.log(topFive);
