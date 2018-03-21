0f44bd4979ccac0bab0e9f160a5a632b56721690
simvirk5 <simvirk5@gmail.com>
2018-03-21 16:17:39 -0700

    class lab

---
+++ js/Answer5.js
@@ -0,0 +1,29 @@
+var myFriends = [
+  'Rickon',
+  'Meera',
+  'Hodor',
+  'Jojen',
+  'Osha',
+  'Rickard',
+  'Maester',
+  'Rodrik',
+  'Jory',
+  'Septa',
+  'Jon'
+];
+
+var yourFriends = [
+  'Bilbo',
+  'Boromir',
+  'Elrond',
+  'Faramir',
+  'Frodo',
+  'Gandalf',
+  'Legolas',
+  'Pippin'
+];
+var totalFriends = myFriends.concat(yourFriends);
+
+console.log(totalFriends);
+
+console.log(totalFriends.sort());
---
+++ js/Answer7.js
@@ -0,0 +1,5 @@
+foods.indexOf('Donut')
+foods.splice(foods.indexOf('Donut'),1)
+
+var list=food.slice(4,9);
+console.log(list);
